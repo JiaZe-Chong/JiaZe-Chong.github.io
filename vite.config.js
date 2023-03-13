d6bd967390dd356e7062b9751be35f787c6de727
JiaZe-Chong <xavier02@iastate.edu>
2023-03-13 11:52:33 -0500

    added export

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+module.exports = {
+  publicPath: '/About-Me/'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
