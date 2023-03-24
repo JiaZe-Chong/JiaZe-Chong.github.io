6a3e93937ef0b30ad53acd885a5dce93d4ba37b9
JiaZe-Chong <xavier02@iastate.edu>
2023-03-24 17:36:50 -0500

    Added work experience

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import projects from './components/ProjectList.vue'
 import intro from './components/Introduction.vue'
 import contact from './components/ContactMe.vue'
+import work from './components/ExperienceList.vue'
 </script>
 
 <template>
     <div class = "all">
         <intro />
+        <work />
         <projects />
         <contact />
     </div>
@@ -15,8 +17,8 @@ import contact from './components/ContactMe.vue'
 
 <style scoped>
     .all{
-        width: 80vw;
-        left: 10vw;
-        right: 10vw;
+        width: 70vw;
+        left: 15vw;
+        right: 15vw;
     }
 </style>
